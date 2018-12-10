b60b69f47c9605d2067e27e86fdc302591d03607
Farah Rizal <41633573+farahriz@users.noreply.github.com>
2018-12-10 15:24:23 +0800

    'Practical' uses for javascript learned

---
+++ buildingInteractiveFeatures/hangmanGame/hangman.js
@@ -0,0 +1,115 @@
+// VARIABLES FOR GAME LOGIC
+const wordToGuess = 'potato';
+const wordState = [undefined, undefined, undefined, undefined, undefined, undefined]
+let guessesLeft = 10;
+const prevGuesses = [ ];
+
+// HTML ELEMENTS
+const wordInProgress = document.getElementById('word')
+const lettersGuessed = document.getElementById('past-guesses')
+const livesLeft = document.getElementById('lives-left')
+const submitForm = document.getElementById('submission')
+const myInput = document.getElementById('my-input')
+const winLoseMessage = document.getElementById('victory-banner')
+const myTrap = document.querySelector('.trap')
+
+// FUNCTIONS
+
+submitForm.onsubmit = function(event){
+	event.preventDefault();
+	checkInput(myInput.value);
+	myInput.value = '';
+}
+	
+
+function displayWordState(state) {
+	let output = '';
+	// Construct new word state
+	for(let i = 0; i < state.length; i++){
+		const char = state[i];
+		if (char!=undefined){
+			output = output + char
+		} else {
+			output = output + '_'
+		}
+	}
+	// replace HTML for game in progress
+	wordInProgress.innerHTML = output;
+}
+
+displayWordState(wordState)
+
+
+//Victory Condition
+function checkVictory(){
+	if (!(wordState.includes(undefined)) && guessesLeft > 0){
+		winLoseMessage.classList.remove('hidden')
+		// Update HTML for win event
+		makeItRain();
+
+		return
+	} else if (guessesLeft == 0) {
+		// Update HTML for loss event
+		winLoseMessage.innerHTML= 'You lost! Better luck next time! Check the console if you are very curious.';
+		myTrap.setAttribute("style", "background-color: red")
+		winLoseMessage.classList.remove('hidden')
+		console.log('The answer was '+ "'" + wordToGuess + "'")
+	} else {
+		// console.log('Hang in there!')
+		// don't do anything to the HTML
+	}
+}
+
+// update word state for new guesses
+function updateWordState(letter){	
+	const positionArray = [];
+	for (let i = 0; i < wordToGuess.length; i++){
+		if (letter==wordToGuess.charAt(i)) {
+			positionArray.push(i);
+		}
+	}
+	for (let i = 0; i < positionArray.length; i++){
+		wordState[positionArray[i]] = letter;
+	}
+}
+
+
+function addGuess(letter){
+	prevGuesses.push(letter)
+	//if guess isn't a letter in wordToGuess, subtract a life
+	if (wordToGuess.includes(letter)===false){
+		guessesLeft = guessesLeft - 1;
+
+	} else {
+		// replace correctly guessed letter into the word state
+		updateWordState(letter);
+	}
+
+	//update HTML for lives left section
+	displayWordState(wordState);		
+	checkVictory();
+	livesLeft.innerHTML = guessesLeft;
+
+	//add to your list of guessed
+	const newGuess = document.createElement('li');
+	newGuess.innerHTML = letter;
+	lettersGuessed.appendChild(newGuess); //append a child
+}
+
+
+
+// prevent user from submitting invalid input
+function checkInput(string){
+	const str = string.toLowerCase();
+	// There has to be a better way to check it's a letter between A-Z, right?
+	const alphabet = ['a', 'b', 'c', 'd','e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	// is string 1 character and a letter between a to z?
+	if (alphabet.includes(str) && !prevGuesses.includes(str)) {
+		//has it been guessed before
+		addGuess(string);
+	} else {
+		window.alert("This is not a valid guess. Make sure you only guess single letters, and make sure it's not something you guessed already.")
+		return
+	}
+}
+
--- buildingInteractiveFeatures/imageGallery/lightbox.js
+++ buildingInteractiveFeatures/imageGallery/lightbox.js
@@ -1,1 +1,55 @@
-console.log("Connected")
+const closeButton = document.querySelector('.lightbox-close');
+const lightbox = document.querySelector('.lightbox');
+const galleryItems = document.querySelectorAll('.gallery-item')
+const lightboxImage = document.querySelector('.lightbox-image');
+const lightboxPrev = document.querySelector('.lightbox-prev');
+const lightboxNext = document.querySelector('.lightbox-next');
+
+
+function hideImage (event){
+	event.preventDefault();
+	lightbox.classList.add('hidden');
+}
+
+function showImage(event){
+	// show lightbox
+	lightbox.classList.remove('hidden');
+
+	// get current galleryItem
+	const elementClickedOn = event.target;
+	const galleryItemParent = elementClickedOn.parentElement;
+
+	// replace contents lightbox-image with current image
+	lightboxImage.innerHTML = galleryItemParent.innerHTML;
+}
+
+
+
+// hide lightbox when close button is clicked
+closeButton.onclick = hideImage;
+
+
+// for every gallery item, set onclick handler to gallery image
+for(let i = 0; i < galleryItems.length; i++){
+	let item = galleryItems[i];
+	item.onclick = showImage;
+}
+
+
+// Previous and Next Button Functionality
+		// 	console.log(lightboxImage.innerHTML);
+		// 	console.log(galleryItems[0].innerHTML)
+
+		// for(let i = 0; i < galleryItems.length; i++){
+		// 	if (lightboxImage.innerHTML == galleryItems[i].innerHTML) {
+		// 		console.log("It's a match!")
+		// 	}
+		// }	
+		// // When the lightbox is not hidden, figure out which image in an array we are at
+		// if 0, no prev
+		// if next, replace with next item in the array
+		// if previous, replace with previous item in the array
+
+		// if last, no next
+
+
